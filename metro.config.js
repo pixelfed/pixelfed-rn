e936f2f89662cf3c9c8e8367f9c4431741ad2c8b
Daniel Supernault <877217+dansup@users.noreply.github.com>
2024-04-07 16:00:59 -0600

    ✨ hello tamagui

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,16 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+let config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: false,
+})
 
-module.exports = config;
+// 2. Enable Tamagui
+const { withTamagui } = require('@tamagui/metro-plugin')
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+  outputCSS: './tamagui-web.css',
+})
