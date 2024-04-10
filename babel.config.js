9d7b101e1bf2f387e4b2c624850a82c764a74215
Daniel Supernault <danielsupernault@gmail.com>
2024-04-10 02:27:20 -0600

    Add expo-image, mmkv. Fix fonts and paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,18 @@ module.exports = (api) => {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: './src',
+            '@state': './src/state',
+            '@components': './src/components',
+            '@requests': './src/requests.js',
+          },
+        },
+      ],
+    ],
   }
 }
