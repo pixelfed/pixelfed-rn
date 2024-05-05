0b06f8bd987c5a84543c1c3e0dd1c5f91bbddc82
Daniel Supernault <danielsupernault@gmail.com>
2024-05-05 03:29:44 -0600

    Add hooks, seeking captain ⚓️

---
+++ src/hooks/useAppState.ts
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import { AppState, type AppStateStatus } from 'react-native'
+
+export function useAppState(onChange: (status: AppStateStatus) => void) {
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onChange)
+    return () => {
+      subscription.remove()
+    }
+  }, [onChange])
+}
