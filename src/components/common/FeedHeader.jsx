7a7dc92a1fc1c067d24f8db4f328a39ff85d4f29
Daniel Supernault <danielsupernault@gmail.com>
2024-05-10 21:58:25 -0600

    Add FeedHeader component

---
+++ src/components/common/FeedHeader.jsx
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState } from 'react'
+import { FlatList, StyleSheet, Pressable, ActivityIndicator } from 'react-native'
+import { Text, View, XStack, Select, Adapt, Sheet, SheetContents } from 'tamagui'
+import FeedPost from 'src/components/post/FeedPost'
+import { StatusBar } from 'expo-status-bar'
+import { Feather } from '@expo/vector-icons'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link, Stack } from 'expo-router'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { fetchHomeFeed } from 'src/lib/api'
+
+export default (FeedHeader = ({ title = 'Home' }) => {
+  return (
+    <XStack
+      px="$3"
+      pb="$3"
+      bg="white"
+      justifyContent="space-between"
+      alignItems="center"
+      zIndex={100}
+    >
+      <XStack alignItems="center" gap="$1">
+        <Text fontSize={30} fontWeight="bold" letterSpacing={-1}>
+          {title}
+        </Text>
+      </XStack>
+      <XStack gap="$5">
+        <Link href="/notifications" asChild>
+          <Pressable>
+            <Feather name="heart" size={26} />
+          </Pressable>
+        </Link>
+        <Link href="/chats" asChild>
+          <Pressable>
+            <Feather name="mail" size={26} />
+          </Pressable>
+        </Link>
+        <Link href="/search" asChild>
+          <Pressable>
+            <Feather name="search" size={26} />
+          </Pressable>
+        </Link>
+      </XStack>
+    </XStack>
+  )
+})
