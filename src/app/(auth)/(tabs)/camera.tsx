79a5d91bce494753110f11e261ef749fb29d5d63
Daniel Supernault <danielsupernault@gmail.com>
2024-05-13 02:04:34 -0600

    Add expo-camera

--- src/app/(auth)/(tabs)/camera.tsx
+++ src/app/(auth)/(tabs)/camera.tsx
@@ -1,25 +1,72 @@
 import { SafeAreaView } from 'react-native'
 import { Button, Text, View, YStack } from 'tamagui'
 import { Feather } from '@expo/vector-icons'
+import { CameraView, useCameraPermissions } from 'expo-camera/next'
+import { useState } from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
 
-export default function CameraScreen() {
+export default function App() {
+  const [facing, setFacing] = useState('back')
+  const [permission, requestPermission] = useCameraPermissions()
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    )
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === 'back' ? 'front' : 'back'))
+  }
+
   return (
-    <SafeAreaView flex={1} alignItems="center" justifyContent="center">
-      <YStack gap="$4">
-        <Button variant="outlined">
-          <Feather name="camera" size={25} color="black" />
-          <Text ml="$6" fontSize="$6">
-            Take Photo
-          </Text>
-        </Button>
-
-        <Button variant="outlined">
-          <Feather name="film" size={25} color="black" />
-          <Text ml="$3" fontSize="$6">
-            Upload photo
-          </Text>
-        </Button>
-      </YStack>
+    <SafeAreaView flex={1}>
+      <View style={styles.container}>
+        <CameraView style={styles.camera} facing={facing}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+})
