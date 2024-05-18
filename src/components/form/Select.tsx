33c945dcac49c51a553f60bb709773a72bc63406
Daniel Supernault <danielsupernault@gmail.com>
2024-05-18 01:46:13 -0600

    Add Select component

---
+++ src/components/form/Select.tsx
@@ -0,0 +1,125 @@
+import Feather from '@expo/vector-icons/Feather'
+import { useEffect, useMemo, useState } from 'react'
+import type { FontSizeTokens, SelectProps } from 'tamagui'
+import { Adapt, Label, Select, Separator, Sheet, XStack, YStack, getFontSize, Text } from 'tamagui'
+import { Animated } from "react-native";
+
+export function FormSelect(props: any) {
+  return (
+    <YStack flexGrow={1} gap="$4">
+      <XStack alignItems='center' justifyContent='space-between' gap="$4">
+        <Label miw={80} fontSize="$5">
+          {props?.label}
+        </Label>
+        <FormSelectItem options={props?.options} defaultValue={props?.defaultValue} open={props.open} />
+      </XStack>
+    </YStack>
+  )
+}
+
+export function FormSelectItem(props: SelectProps) {
+  const [val, setVal] = useState(props?.defaultValue)
+
+  return (
+    <Select value={val} onValueChange={setVal} disablePreventBodyScroll {...props}>
+      <Select.Trigger width={220} iconAfter={<Feather name="chevron-down" />}>
+        <Select.Value placeholder="Select an option" />
+      </Select.Trigger>
+
+      <Adapt when="sm" platform="touch">
+        <Sheet
+          modal
+          dismissOnSnapToBottom
+          animationConfig={{
+            type: 'spring',
+            damping: 20,
+            mass: 1.2,
+            stiffness: 250,
+          }}
+        >
+          <Sheet.Frame>
+            <Sheet.ScrollView>
+              <Adapt.Contents />
+            </Sheet.ScrollView>
+          </Sheet.Frame>
+          <Sheet.Overlay
+            animation="lazy"
+            enterStyle={{ opacity: 0 }}
+            exitStyle={{ opacity: 0 }}
+          />
+        </Sheet>
+      </Adapt>
+
+      <Select.Content zIndex={200000}>
+        <Select.ScrollUpButton
+          alignItems="center"
+          justifyContent="center"
+          position="relative"
+          width="100%"
+          height="$3"
+        >
+          <YStack zIndex={10}>
+            <Feather name="chevron-up" size={20} />
+          </YStack>
+        </Select.ScrollUpButton>
+
+        <Select.Viewport
+          // to do animations:
+          animation="quick"
+          animateOnly={['transform', 'opacity']}
+          enterStyle={{ o: 0, y: -10 }}
+          exitStyle={{ o: 0, y: 10 }}
+        >
+          <Select.Group>
+            {/* <Select.Label>Select</Select.Label> */}
+            {/* for longer lists memoizing these is useful */}
+            {useMemo(
+              () =>
+                props?.options?.map((item, i) => {
+                  return (
+                    <>
+                      <Separator />
+                      <Select.Item
+                        index={i}
+                        key={item.name}
+                        value={item.name}
+                        py="$3"
+                        alignItems='center'
+                        >
+                          <YStack flexGrow={1} py="$2">
+                            <XStack>
+                              <Select.ItemText flexWrap='wrap' fontSize="$6" fontWeight={val == item.name ? 'bold' : 'normal'}>
+                                { item.name }
+                              </Select.ItemText>
+                              <Select.ItemIndicator marginLeft="auto">
+                                <Feather name="check" size={16} />
+                              </Select.ItemIndicator>
+                            </XStack>
+                          </YStack>
+                        </Select.Item>
+                    </>
+                  )
+                }),
+              [props, val]
+            )}
+          </Select.Group>
+        </Select.Viewport>
+
+        <Select.ScrollDownButton
+          alignItems="center"
+          justifyContent="center"
+          position="relative"
+          width="100%"
+          height="$3"
+        >
+          <YStack zIndex={10}>
+            <Feather 
+                name="chevron-down" 
+                size={20} 
+              />
+          </YStack>
+        </Select.ScrollDownButton>
+      </Select.Content>
+    </Select>
+  )
+}
