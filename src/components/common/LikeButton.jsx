f4e1ea2267bf5e957d1a3a5bef283207bbb501b8
Daniel Supernault <danielsupernault@gmail.com>
2024-06-07 17:32:35 -0600

    Create LikeButton.jsx

---
+++ src/components/common/LikeButton.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  Extrapolate,
+  interpolate,
+} from "react-native-reanimated";
+import { Pressable, View, Button, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as Haptics from 'expo-haptics';
+
+export default function LikeButton (props) {
+  const liked = useSharedValue(props.hasLiked ? 1 : 0);
+
+  const outlineStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: interpolate(liked.value, [0, 1], [1, 0], Extrapolate.CLAMP),
+        },
+      ],
+    };
+  });
+
+  const fillStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: liked.value,
+        },
+      ],
+      opacity: liked.value,
+    };
+  });
+
+  const handleLike = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+    liked.value = withSpring(liked.value ? 0 : 1)
+    props.handleLike()
+  }
+
+  return (
+    <Pressable onPress={() => handleLike()}>
+      <Animated.View style={[StyleSheet.absoluteFillObject, outlineStyle]}>
+        <MaterialCommunityIcons
+          name={"heart-outline"}
+          size={29}
+          color={"black"}
+        />
+      </Animated.View>
+
+      <Animated.View style={fillStyle}>
+        <MaterialCommunityIcons name={"heart"} size={29} color={"red"} />
+      </Animated.View>
+    </Pressable>
+  );
+};
