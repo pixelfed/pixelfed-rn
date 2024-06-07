82d574cf6d38994a50ac049c48bb264b5db5b476
Daniel Supernault <danielsupernault@gmail.com>
2024-06-07 17:32:44 -0600

    Update UserAvatar.tsx

--- src/components/common/UserAvatar.tsx
+++ src/components/common/UserAvatar.tsx
@@ -1,13 +1,13 @@
 import { Avatar } from 'tamagui'
 
-export default function UserAvatar({ url, width, height }) {
+export default function UserAvatar({ url, width = 40, height = 40}) {
   return (
     <Avatar circular borderWidth={1} borderColor="$gray6">
       <Avatar.Image
         source={{
           uri: url,
-          width: 40,
-          height: 40,
+          width: width,
+          height: height,
         }}
       />
       <Avatar.Fallback backgroundColor="$gray6" />
