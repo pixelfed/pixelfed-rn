34629615e9a0efaa22e574d3a35db1e77a4359de
Daniel Supernault <danielsupernault@gmail.com>
2024-06-07 17:35:49 -0600

    Update CommentFeed.jsx

--- src/components/post/CommentFeed.jsx
+++ src/components/post/CommentFeed.jsx
@@ -5,15 +5,16 @@ import {
   Pressable,
   ActivityIndicator,
   Dimensions,
+  Alert,
 } from 'react-native'
 import { Text, View, XStack, Select, Adapt, Sheet, YStack, Separator } from 'tamagui'
 import FeedPost from 'src/components/post/FeedPost'
 import { StatusBar } from 'expo-status-bar'
-import { Feather } from '@expo/vector-icons'
+import { Feather, Ionicons } from '@expo/vector-icons'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Link, Stack } from 'expo-router'
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
-import { getStatusRepliesById } from 'src/lib/api'
+import { useInfiniteQuery, useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getStatusRepliesById, postComment, likeStatus, unlikeStatus, deleteStatus } from 'src/lib/api'
 import FeedHeader from 'src/components/common/FeedHeader'
 import { Storage } from 'src/state/cache'
 import {
@@ -25,45 +26,115 @@ import {
   BottomSheetTextInput,
 } from '@gorhom/bottom-sheet'
 import UserAvatar from '../common/UserAvatar'
-import { _timeAgo } from 'src/utils'
+import { _timeAgo, likeCountLabel, prettyCount } from 'src/utils'
+import ReadMore from '../common/ReadMore'
 
 const SCREEN_WIDTH = Dimensions.get('screen').width
 
-export default function CommentFeed({ id }) {
+export default function CommentFeed({ id, showLikes, user, handleReport, gotoProfile }) {
   const [commentText, setComment] = useState()
+  const queryClient = useQueryClient()
+  const commentRef = useRef()
+
+  const handlePost = (nativeEvent) => {
+    setComment()
+    commentMutation.mutate({postId: id, commentText: commentText})
+  }
+
+  const handleShowLikes = (id) => {
+    showLikes(id)
+  }
+
+  const handleCommentLike = (item) => {
+    if(item.favourited) {
+      likeMutation.mutate({id: item.id, type: 'unlike'})
+      return
+    }
+    likeMutation.mutate({id: item.id, type: 'like'})
+  }
+
+  const handleCommentReport = (id) => {
+    handleReport(id)
+  }
+
+  const handleCommentDelete = (id) => {
+    Alert.alert(
+      'Confirm Delete',
+      'Are you sure you want to delete your comment?',
+      [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Delete',
+          style: 'destructive',
+          onPress: () => commentDeleteMutation({id: id})
+        }
+      ]
+    )
+  }
+
   const renderItem = useCallback(
     ({ item }) => (
       <View style={styles.itemContainer}>
         <YStack flexShrink={1}>
           <XStack flexShrink={1}>
             <XStack gap="$3" flexGrow={1}>
-              <UserAvatar url={item.account.avatar} width={30} height={30} />
+              <Pressable onPress={() => gotoProfile(item.account.id)}>
+                <UserAvatar url={item.account.avatar} width={30} height={30} />
+              </Pressable>
 
               <YStack flexGrow={1} maxWidth={SCREEN_WIDTH - 150} gap={4}>
                 <XStack gap="$2">
-                  <Text fontSize="$3" fontWeight="bold">
-                    {item.account.acct}
-                  </Text>
+                  <Pressable onPress={() => gotoProfile(item?.account.id)}>
+                    <Text fontSize="$3" fontWeight="bold">
+                      {item.account.acct}
+                    </Text>
+                  </Pressable>
                   <Text fontSize="$3" color="$gray9">
                     {_timeAgo(item.created_at)}
                   </Text>
                 </XStack>
-                <Text flexWrap="wrap" fontSize="$4">
-                  {item.content_text}
-                </Text>
-                {/* <Pressable>
-                  <Text mt="$2" fontWeight="bold" fontSize="$3" color="$gray9">
-                    Reply
+                <ReadMore numberOfLines={3}>
+                  <Text mt={3} flexWrap="wrap" fontSize="$4">
+                    {item.content_text}
                   </Text>
-                </Pressable> */}
+                </ReadMore>
+                <XStack mt="$2" gap="$4">
+                  <Pressable onPress={() => commentRef?.current.focus()}>
+                    <Text fontWeight="bold" fontSize="$3" color="$gray9">
+                      Reply
+                    </Text>
+                  </Pressable>
+                  { item.favourites_count ? 
+                  <Pressable onPress={() => handleShowLikes(item.id)}>
+                  <Text fontSize="$3" color="$gray9">
+                    { likeCountLabel(item?.favourites_count) }
+                  </Text>
+                </Pressable> : null }
+                { item.account.id != user?.id ? 
+                <Pressable onPress={() => handleCommentReport(item?.id)}>
+                  <Text fontSize="$3" color="$gray9">Report</Text>
+                </Pressable>
+                : 
+                <Pressable onPress={() => handleCommentDelete(item.id)}>
+                  <Text fontSize="$3" color="$gray9">Delete</Text>
+                </Pressable>
+                }
+                </XStack>
               </YStack>
             </XStack>
-            <YStack justifyContent="center" alignItems="center" gap="$1">
-              <Feather name="heart" size={15} />
-              {item.favourites_count ? (
-                <Text fontSize="$3">{item.favourites_count}</Text>
-              ) : null}
-            </YStack>
+            <Pressable onPress={() => handleCommentLike(item)}>
+              <YStack justifyContent="center" alignItems="center" gap="$1">
+                { item.favourited ?
+                  <Ionicons name="heart" color="red" size={20} /> :
+                  <Ionicons name="heart-outline" color="black" size={20} />
+                }
+                {item.favourites_count ? (
+                  <Text fontSize="$3">{prettyCount(item.favourites_count)}</Text>
+                ) : null}
+              </YStack>
+            </Pressable>
           </XStack>
           {item.replies_count ? (
             <Pressable>
@@ -92,6 +163,48 @@ export default function CommentFeed({ id }) {
     )
   }, [])
 
+  const RenderEmpty = useCallback(() => {
+    return (
+      <View flexGrow={1} justifyContent='center' alignItems='center'>
+        <YStack justifyContent='center' alignItems='center' gap="$3">
+          <Text fontSize="$9" fontWeight="bold">No comments yet</Text>
+          <Text fontSize="$6" color="$gray9">Start the conversation</Text>
+        </YStack>
+      </View>
+    )
+  })
+
+  const commentMutation = useMutation({
+    mutationFn: (newComment) => {
+      return postComment(newComment)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['getStatusRepliesById'] })
+    }
+  })
+
+  const likeMutation = useMutation({
+    mutationFn: (handleLike) => {
+      return handleLike.type === 'like' ? likeStatus(handleLike) : unlikeStatus(handleLike)
+    },
+    onSuccess: () => {
+      setTimeout(() => {
+        queryClient.invalidateQueries({ queryKey: ['getStatusRepliesById'] })
+      }, 500)
+    }
+  })
+
+  const { isPending, mutate: commentDeleteMutation } = useMutation({
+    mutationFn: (commentDelete) => {
+      return deleteStatus(commentDelete)
+    },
+    onSuccess: () => {
+      setTimeout(() => {
+        queryClient.invalidateQueries({ queryKey: ['getStatusRepliesById'] })
+      }, 1500)
+    }
+  })
+
   const {
     data,
     fetchNextPage,
@@ -105,6 +218,7 @@ export default function CommentFeed({ id }) {
   } = useInfiniteQuery({
     queryKey: ['getStatusRepliesById', id],
     initialPageParam: null,
+    refetchOnWindowFocus: false,
     queryFn: async ({ pageParam }) => {
       const data = await getStatusRepliesById(id, 0)
       return data
@@ -135,13 +249,18 @@ export default function CommentFeed({ id }) {
         keyExtractor={(i) => i?.id}
         renderItem={renderItem}
         ListHeaderComponent={RenderHeader}
+        ListEmptyComponent={RenderEmpty}
+        showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.contentContainer}
       />
       <BottomSheetTextInput
+        ref={commentRef}
         style={styles.input}
         value={commentText}
         onChangeText={setComment}
-        multiline={true}
+        returnKeyType='send'
+        returnKeyLabel='Post'
+        onSubmitEditing={handlePost}
         placeholder="Add a comment..."
       />
     </>
@@ -153,6 +272,7 @@ const styles = StyleSheet.create({
     paddingTop: 200,
   },
   contentContainer: {
+    flexGrow: 1,
     backgroundColor: 'white',
   },
   itemContainer: {
@@ -168,12 +288,21 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
     marginBottom: 30,
     borderRadius: 10,
-    minHeight: 50,
     fontSize: 20,
-    lineHeight: 35,
     padding: 10,
     borderWidth: 1,
     borderColor: 'rgba(151, 151, 151, 0.25)',
     backgroundColor: 'rgba(151, 151, 151, 0.05)',
+  },
+  footerContainer: {
+    padding: 12,
+    margin: 12,
+    borderRadius: 12,
+    backgroundColor: '#80f',
+  },
+  footerText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: '800',
   },
 })
