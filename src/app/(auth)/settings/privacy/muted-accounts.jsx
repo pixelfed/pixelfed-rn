a7cba7f28b528a5f4b493084d729489717df6590
Daniel Supernault <danielsupernault@gmail.com>
2024-06-25 22:42:43 -0600

    Update muted-accounts.jsx

--- src/app/(auth)/settings/privacy/muted-accounts.jsx
+++ src/app/(auth)/settings/privacy/muted-accounts.jsx
@@ -9,24 +9,35 @@ import FastImage from 'react-native-fast-image'
 
 export default function Page() {
   const RenderItem = ({ item }) => (
-    <XStack px="$5" py="$3" bg="white" alignItems="center" gap="$3" flexWrap="wrap">
-      <FastImage
-        source={{ uri: item?.avatar }}
-        style={{
-          width: 40,
-          height: 40,
-          borderRadius: 40,
-          borderWidth: 1,
-          borderColor: '#ccc',
-        }}
-      />
-      <Text fontWeight={'bold'} flexShrink={1} maxWidth={'80%'}>
-        {item.acct}
-      </Text>
-    </XStack>
+    <Link href={`/profile/${item.id}`} asChild>
+      <XStack px="$5" py="$3" bg="white" alignItems="center" gap="$3" flexWrap="wrap">
+        <FastImage
+          source={{ uri: item?.avatar }}
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 40,
+            borderWidth: 1,
+            borderColor: '#ccc',
+          }}
+          />
+        <Text fontWeight={'bold'} flexShrink={1} maxWidth={'80%'}>
+          {item.acct}
+        </Text>
+      </XStack>
+    </Link>
   )
 
   const RenderSeparator = () => <Separator />
+
+  const RenderEmpty = () => (
+    <View flexGrow={1} justifyContent="center" alignItems="center" py="$5">
+      <YStack flexShrink={1} justifyContent="center" alignItems="center" gap="$5">
+        <Feather name="alert-circle" size={70} />
+        <Text fontSize="$7" allowFontScaling={false}>No results found</Text>
+      </YStack>
+    </View>
+  )
 
   const { isPending, isError, data, error } = useQuery({
     queryKey: ['mutedAccounts'],
@@ -45,6 +56,8 @@ export default function Page() {
         data={data}
         renderItem={RenderItem}
         ItemSeparatorComponent={RenderSeparator}
+        ListEmptyComponent={RenderEmpty}
+        contentContainerStyle={{flexGrow: 1}}
       />
     </SafeAreaView>
   )
