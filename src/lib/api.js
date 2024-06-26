f6a004f8a5aafa02038aea5ca1d01e2b45c5c533
Daniel Supernault <danielsupernault@gmail.com>
2024-06-26 00:46:33 -0600

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,25 @@
 import { Storage } from 'src/state/cache'
 
+export function randomKey(length) {
+  let result = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  let counter = 0
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+  return result
+}
+
+export function objectToForm(obj) {
+  let form = new FormData()
+
+  Object.keys(obj).forEach((key) => form.append(key, obj[key]))
+
+  return form
+}
+
 export function parseLinkHeader(header) {
   if (!header || header.length === 0) {
     return null
@@ -15,6 +35,116 @@ export function parseLinkHeader(header) {
   })
 
   return links
+}
+
+export async function _selfPost(
+  url,
+  token,
+  params = {},
+  asForm = false,
+  rawRes = false,
+  idempotency = false
+) {
+  let headers = {}
+  headers['Authorization'] = `Bearer ${token}`
+  headers['Accept'] = 'application/json'
+  headers['Content-Type'] = asForm ? 'multipart/form-data' : 'application/json'
+
+  if (idempotency) {
+    headers['Idempotency-Key'] = randomKey(40)
+  }
+
+  const resp = await fetch(url, {
+    method: 'POST',
+    body: asForm ? objectToForm(params) : JSON.stringify(params),
+    headers,
+  })
+
+  return rawRes ? resp : resp.json()
+}
+
+export async function selfPost(
+  path,
+  params = {},
+  asForm = false,
+  rawRes = false,
+  idempotency = false
+) {
+  let headers = {}
+  const instance = Storage.getString('app.instance')
+  const token = Storage.getString('app.token')
+  const url = `https://${instance}/${path}`
+
+  headers['Authorization'] = `Bearer ${token}`
+  headers['Accept'] = 'application/json'
+  headers['Content-Type'] = asForm ? 'multipart/form-data' : 'application/json'
+
+  if (idempotency) {
+    headers['Idempotency-Key'] = randomKey(40)
+  }
+
+  const resp = await fetch(url, {
+    method: 'POST',
+    body: asForm ? objectToForm(params) : JSON.stringify(params),
+    headers,
+  })
+
+  return rawRes ? resp : resp.json()
+}
+
+export async function selfDelete(
+  path,
+  params = {},
+  rawRes = false,
+  idempotency = false
+) {
+  let headers = {}
+  const instance = Storage.getString('app.instance')
+  const token = Storage.getString('app.token')
+  const url = `https://${instance}/${path}`
+
+  headers['Authorization'] = `Bearer ${token}`
+  headers['Accept'] = 'application/json'
+  headers['Content-Type'] = 'application/json'
+
+  if (idempotency) {
+    headers['Idempotency-Key'] = randomKey(40)
+  }
+
+  const resp = await fetch(url, {
+    method: 'DELETE',
+    body: JSON.stringify(params),
+    headers,
+  })
+
+  return rawRes ? resp : resp.json()
+}
+
+export async function selfGet(
+  path,
+  params = {},
+  rawRes = false,
+  idempotency = false
+) {
+  let headers = {}
+  const instance = Storage.getString('app.instance')
+  const token = Storage.getString('app.token')
+  const url = `https://${instance}/${path}`
+
+  headers['Authorization'] = `Bearer ${token}`
+  headers['Accept'] = 'application/json'
+  headers['Content-Type'] = 'application/json'
+
+  if (idempotency) {
+    headers['Idempotency-Key'] = randomKey(40)
+  }
+
+  const resp = await fetch(url, {
+    method: 'GET',
+    headers,
+  })
+
+  return rawRes ? resp : resp.json()
 }
 
 async function fetchPaginatedData(url) {
@@ -78,6 +208,10 @@ export async function searchQuery(query) {
     }),
     ...data.accounts.map((a) => {
       a._type = 'account'
+      return a
+    }),
+    ...data.statuses.map((a) => {
+      a._type = 'status'
       return a
     }),
   ]
@@ -149,6 +283,37 @@ export async function getAccountById({ queryKey }) {
   return await fetchData(url)
 }
 
+export async function followAccountById(id) {
+  let path = `api/v1/accounts/${id}/follow`
+  return await selfPost(path)
+}
+
+export async function unfollowAccountById(id) {
+  let path = `api/v1/accounts/${id}/unfollow`
+  return await selfPost(path)
+}
+
+export async function reportProfile({ id, type }) {
+  const instance = Storage.getString('app.instance')
+  const token = Storage.getString('app.token')
+
+  const params = new URLSearchParams({
+    report_type: type,
+    object_type: 'user',
+    object_id: id,
+  })
+  const url = `https://${instance}/api/v1.1/report?${params}`
+  const response = await fetch(url, {
+    method: 'post',
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }),
+  })
+  return await response.json()
+}
+
 export async function getAccountByUsername({ queryKey }) {
   const instance = Storage.getString('app.instance')
   const url = `https://${instance}/api/v1.1/accounts/username/${queryKey[1]}?_pe=1`
@@ -157,7 +322,7 @@ export async function getAccountByUsername({ queryKey }) {
 
 export async function getAccountStatusesById(id, page) {
   const instance = Storage.getString('app.instance')
-  const url = `https://${instance}/api/v1/accounts/${id}/statuses?_pe=1&max_id=${page}`
+  const url = `https://${instance}/api/v1/accounts/${id}/statuses?_pe=1&limit=24&max_id=${page}`
   return await fetchData(url)
 }
 
@@ -182,6 +347,17 @@ export async function getHashtagRelated(id) {
 export async function getConversations(params) {
   const instance = Storage.getString('app.instance')
   let url = `https://${instance}/api/v1/conversations`
+  return await fetchData(url)
+}
+
+export async function getComposeSettings() {
+  const path = `api/v1.1/compose/settings`
+  return await selfGet(path)
+}
+
+export async function getConfig() {
+  const instance = Storage.getString('app.instance')
+  let url = `https://${instance}/api/v2/config`
   return await fetchData(url)
 }
 
@@ -247,7 +423,7 @@ export async function getTrendingPopularPosts() {
 
 export async function getAccountRelationship({ queryKey }) {
   const instance = Storage.getString('app.instance')
-  let url = `https://${instance}/api/v1/accounts/relationships?id[]=${queryKey[1]}`
+  let url = `https://${instance}/api/v1/accounts/relationships?id[]=${queryKey[1]}&_pe=1`
   const res = await fetchData(url)
   return res[0]
 }
@@ -347,10 +523,30 @@ export async function getMutes() {
   return await fetchData(url)
 }
 
+export async function muteProfileById(id) {
+  let path = `api/v1/accounts/${id}/mute`
+  return await selfPost(path)
+}
+
+export async function unmuteProfileById(id) {
+  let path = `api/v1/accounts/${id}/unmute`
+  return await selfPost(path)
+}
+
 export async function getBlocks() {
   const instance = Storage.getString('app.instance')
   let url = `https://${instance}/api/v1/blocks`
   return await fetchData(url)
+}
+
+export async function blockProfileById(id) {
+  let path = `api/v1/accounts/${id}/block`
+  return await selfPost(path)
+}
+
+export async function unblockProfileById(id) {
+  let path = `api/v1/accounts/${id}/unblock`
+  return await selfPost(path)
 }
 
 export async function getSelfCollections() {
@@ -377,6 +573,12 @@ export async function getAppSettings() {
   return await fetchData(url)
 }
 
+export async function getFollowRequests() {
+  const instance = Storage.getString('app.instance')
+  let url = `https://${instance}/api/v1/follow_requests`
+  return await fetchData(url)
+}
+
 export async function getSelfAccount() {
   const instance = Storage.getString('app.instance')
   let url = `https://${instance}/api/v1/accounts/verify_credentials?_pe=1`
@@ -390,6 +592,102 @@ export async function updateCredentials(data) {
   let url = `https://${instance}/api/v1/accounts/update_credentials?${params.toString()}`
   const response = await fetch(url, {
     method: 'patch',
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }),
+  })
+  return await response.json()
+}
+
+export async function updateAvatar(data) {
+  const instance = Storage.getString('app.instance')
+  const token = Storage.getString('app.token')
+  let url = `https://${instance}/api/v1/accounts/update_credentials`
+  return await _selfPost(url, token, data, true)
+}
+
+export async function accountFollowRequestAccept(id) {
+  let path = `api/v1/follow_requests/${id}/authorize`
+  return await selfPost(path)
+}
+
+export async function accountFollowRequestReject(id) {
+  let path = `api/v1/follow_requests/${id}/reject`
+  return await selfPost(path)
+}
+
+export async function deleteAvatar() {
+  const instance = Storage.getString('app.instance')
+  const token = Storage.getString('app.token')
+  let url = `https://${instance}/api/v1.1/accounts/avatar`
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }),
+  })
+  return await response.json()
+}
+
+export async function fetchChatThread(id) {
+  const path = `api/v1.1/direct/thread?pid=${id}`
+  return await selfGet(path)
+}
+
+export async function deleteChatMessage(id) {
+  const path = `api/v1.1/direct/thread/message?id=${id}`
+  return await selfDelete(path)
+}
+
+export async function sendChatMessage(id, message) {
+  const path = `api/v1.1/direct/thread/send`
+  return await selfPost(path, {
+    to_id: id,
+    message: message,
+    type: 'text'
+  })
+}
+
+export async function uploadMediaV2(params) {
+  const path = `api/v2/media`
+  return await selfPost(path, params, true, false, true)
+}
+
+export async function postNewStatus(params) {
+  const path = `api/v1/statuses`
+  return await selfPost(path, params, false, false, true)
+}
+
+export async function getAdminStats() {
+  const path = `api/admin/stats`
+  return await selfGet(path)
+}
+
+export async function adminInstances(queryKey = false, sort, sortBy) {
+  const instance = Storage.getString('app.instance')
+  let path = queryKey.pageParam ? queryKey.pageParam : `https://${instance}/api/admin/instances/list?order_by=sort=${sort}&sort_by=${sortBy}`
+  const res = await fetchData(path);
+  return { data: res.data, nextPage: res.links?.next, prevPage: res.links?.prev }
+}
+
+export async function adminInstanceGet(params) {
+  return await selfGet('api/admin/instances/get', params);
+}
+
+export async function getDomainBlocks(params) {
+  return await selfGet('api/v1/domain_blocks', params);
+}
+
+export async function deleteStatusV1(id) {
+  const instance = Storage.getString('app.instance')
+  const token = Storage.getString('app.token')
+  let url = `https://${instance}/api/v1/statuses/${id}`
+  const response = await fetch(url, {
+    method: 'delete',
     headers: new Headers({
       Authorization: `Bearer ${token}`,
       Accept: 'application/json',
