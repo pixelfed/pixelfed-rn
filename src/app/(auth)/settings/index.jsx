c7dd5e7120fb084b440502f2da4ccb34346edb81
Daniel Supernault <danielsupernault@gmail.com>
2024-07-03 23:02:41 -0600

    Bump version

--- src/app/(auth)/settings/index.jsx
+++ src/app/(auth)/settings/index.jsx
@@ -28,7 +28,7 @@ export default function Page() {
   }, [navigation])
   const [user, setUser] = useState()
   const instance = Storage.getString('app.instance')
-  const buildVersion = 50
+  const buildVersion = 51
   const version = Application.nativeApplicationVersion + '.' + buildVersion
 
   useEffect(() => {
