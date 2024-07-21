6be05e91a8eb3d2a1156fe49741fefa80bfa2ade
Daniel Supernault <danielsupernault@gmail.com>
2024-07-20 22:03:52 -0600

    Update index.jsx

--- src/app/(auth)/settings/index.jsx
+++ src/app/(auth)/settings/index.jsx
@@ -28,7 +28,7 @@ export default function Page() {
   }, [navigation])
   const [user, setUser] = useState()
   const instance = Storage.getString('app.instance')
-  const buildVersion = 65
+  const buildVersion = 66
   const version = Application.nativeApplicationVersion + '.' + buildVersion
 
   useEffect(() => {
