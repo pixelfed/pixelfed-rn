022cfbfb5e5183395e0dfda5e465a7c8dc607ae4
Daniel Supernault <danielsupernault@gmail.com>
2024-12-25 23:22:43 -0700

    wip

--- src/app/(auth)/settings/index.jsx
+++ src/app/(auth)/settings/index.jsx
@@ -28,7 +28,7 @@ export default function Page() {
   }, [navigation])
   const [user, setUser] = useState()
   const instance = Storage.getString('app.instance')
-  const buildVersion = 70
+  const buildVersion = 71
   const version = Application.nativeApplicationVersion + '.' + buildVersion
 
   useEffect(() => {
