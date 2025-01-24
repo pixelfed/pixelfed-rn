c0523757791a0ed5a162ac76300989eee97a5692
Karol Wąsowski <wasowski02@protonmail.com>
2025-01-24 14:34:37 +0100

    Removed unused code and artificial delay in login screen

--- src/app/(public)/login.tsx
+++ src/app/(public)/login.tsx
@@ -1,39 +1,14 @@
-import { Link, router, useNavigation } from 'expo-router'
+import { Link } from 'expo-router'
 import {
-  Text,
   View,
-  Form,
   Button,
   YStack,
   Image,
-  Label,
-  Input,
-  Separator,
-  XStack,
 } from 'tamagui'
-import { useAuth } from '@state/AuthProvider'
-import { ActivityIndicator, Platform, SafeAreaView } from 'react-native'
-import { useEffect, useState } from 'react'
+import { Platform, SafeAreaView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 
 export default function Login() {
-  const [server, setServer] = useState('pixelfed.social')
-  const [loading, setLoading] = useState(true)
-
-  const { login, isLoading } = useAuth()
-
-  const handleLogin = () => {
-    login(server)
-  }
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false)
-    }, 2000)
-
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
     <SafeAreaView
       style={{
@@ -42,39 +17,32 @@ export default function Login() {
         justifyContent: 'center',
         alignItems: 'center',
       }}
-      edges={['top']}
     >
       <StatusBar style="light" />
-      {isLoading || loading ? (
-        <View m="$5">
-          <ActivityIndicator style={'light'} />
+      <YStack flexGrow={1} w="100%" px="$5">
+        <View flexGrow={1} justifyContent="center" alignItems="center">
+          <Image
+            source={require('../../../assets/icon.png')}
+            width={140}
+            height={140}
+          />
         </View>
-      ) : (
-        <YStack flexGrow={1} w="100%" px="$5">
-          <View flexGrow={1} justifyContent="center" alignItems="center">
-            <Image
-              source={require('../../../assets/icon.png')}
-              width={140}
-              height={140}
-            />
-          </View>
 
-          <YStack flexDirection="row" mb={Platform.OS === 'ios' ? 0 : '$5'}>
-            <Link href="/selectServer" asChild>
-              <Button
-                size="$6"
-                theme="gray"
-                themeInverse={true}
-                color="white"
-                fontWeight="bold"
-                flexGrow={1}
-              >
-                Login
-              </Button>
-            </Link>
-          </YStack>
+        <YStack flexDirection="row" mb={Platform.OS === 'ios' ? 0 : '$5'}>
+          <Link href="/selectServer" asChild>
+            <Button
+              size="$6"
+              theme="gray"
+              themeInverse={true}
+              color="white"
+              fontWeight="bold"
+              flexGrow={1}
+            >
+              Login
+            </Button>
+          </Link>
         </YStack>
-      )}
+      </YStack>
     </SafeAreaView>
   )
 }
