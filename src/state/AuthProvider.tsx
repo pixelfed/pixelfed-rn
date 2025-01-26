246dc728619a1114ccc37aca5d76e1773e182ead
Simon Laux <mobile.info@simonlaux.de>
2025-01-27 00:44:49 +0100

    fix issue from last commit

--- src/state/AuthProvider.tsx
+++ src/state/AuthProvider.tsx
@@ -241,7 +241,7 @@ export function useQuerySelfProfile() {
     isFetchedAfterMount,
   } = useQuery<Account>({
     queryKey: ['profileById', userCache.id],
-    queryFn: () => getAccountById(id),
+    queryFn: () => getAccountById(userCache.id),
     placeholderData: userCache,
   })
 
