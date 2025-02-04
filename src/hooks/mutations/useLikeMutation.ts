e850004092e855af47667629421952fd93d2da38
Michele De Cillis <decillismicheledeveloper@gmail.com>
2025-02-04 23:40:18 +0100

    fixed like synchronization across screens

--- src/hooks/mutations/useLikeMutation.ts
+++ src/hooks/mutations/useLikeMutation.ts
@@ -1,5 +1,6 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { likeStatus, unlikeStatus } from 'src/lib/api'
+import { Status } from 'src/lib/api-types'
 
 type onSuccessType = Parameters<typeof useMutation>[0]['onSuccess']
 type LikeMutateType = {
@@ -7,13 +8,22 @@ type LikeMutateType = {
   id: string
 }
 
+
 export function useLikeMutation({ onSuccess }: { onSuccess?: onSuccessType } = {}) {
-  const likeMutation = useMutation({
+  const queryClient = useQueryClient();
+
+  // By default, after a like/unlike I'm updating the state with the new info I received from backend
+  const defaultOnSuccess: onSuccessType = (data: Status, variables) => {
+    queryClient.setQueryData(['getStatusById', data.id], data)
+  };
+
+  const { mutate, isPending } = useMutation({
     mutationFn: async (handleLike: LikeMutateType) => {
       try {
-        return handleLike.type === 'like'
+        const res = handleLike.type === 'like'
           ? await likeStatus(handleLike)
-          : await unlikeStatus(handleLike)
+          : await unlikeStatus(handleLike);
+        return res;
       } catch (error) {
         console.error('Error within mutationFn:', error)
         throw error // the thrown error is not handled? the count is still updated / not reverted in ui
@@ -22,7 +32,7 @@ export function useLikeMutation({ onSuccess }: { onSuccess?: onSuccessType } = {
     onError: (error) => {
       console.error('Error handled by like useMutation:', error)
     },
-    onSuccess,
+    onSuccess: onSuccess || defaultOnSuccess
   })
 
   /**
@@ -34,11 +44,11 @@ export function useLikeMutation({ onSuccess }: { onSuccess?: onSuccessType } = {
    */
   async function handleLike(id: string, liked: boolean) {
     try {
-      likeMutation.mutate({ type: liked ? 'like' : 'unlike', id: id })
+      mutate({ type: liked ? 'like' : 'unlike', id: id })
     } catch (error) {
       console.error('Error occurred during like:', error)
     }
   }
 
-  return { handleLike }
+  return { handleLike, isLikePending: isPending }
 }
