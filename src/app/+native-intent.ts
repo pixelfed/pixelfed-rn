5d552a0335127d6327c63d7243740b249dbe3c04
Daniel Supernault <danielsupernault@gmail.com>
2025-02-05 22:35:18 -0700

    Fix share intent

---
+++ src/app/+native-intent.ts
@@ -0,0 +1,22 @@
+import { getShareExtensionKey } from "expo-share-intent";
+
+export function redirectSystemPath({
+  path,
+  initial,
+}: {
+  path: string;
+  initial: string;
+}) {
+  try {
+    if (path.includes(`dataUrl=${getShareExtensionKey()}`)) {
+      // redirect to the ShareIntent Screen to handle data with the hook
+      console.debug(
+        "[expo-router-native-intent] redirect to ShareIntent screen",
+      );
+      return "/camera";
+    }
+    return path;
+  } catch {
+    return "/";
+  }
+}
