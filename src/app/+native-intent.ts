c1824ab1b5b6c893c3ae1904473304a8a21dcd38
Daniel Supernault <danielsupernault@gmail.com>
2025-02-05 22:58:51 -0700

    Lint

--- src/app/+native-intent.ts
+++ src/app/+native-intent.ts
@@ -1,22 +1,20 @@
-import { getShareExtensionKey } from "expo-share-intent";
+import { getShareExtensionKey } from 'expo-share-intent'
 
 export function redirectSystemPath({
   path,
   initial,
 }: {
-  path: string;
-  initial: string;
+  path: string
+  initial: string
 }) {
   try {
     if (path.includes(`dataUrl=${getShareExtensionKey()}`)) {
       // redirect to the ShareIntent Screen to handle data with the hook
-      console.debug(
-        "[expo-router-native-intent] redirect to ShareIntent screen",
-      );
-      return "/camera";
+      console.debug('[expo-router-native-intent] redirect to ShareIntent screen')
+      return '/camera'
     }
-    return path;
+    return path
   } catch {
-    return "/";
+    return '/'
   }
 }
