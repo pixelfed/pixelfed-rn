368992d81419eec4a3340b2c45bbd2fdea560c4a
Daniel Supernault <danielsupernault@gmail.com>
2025-03-03 19:54:12 -0700

    Update Profile, fix mediaTypes

--- src/app/(auth)/settings/profile.tsx
+++ src/app/(auth)/settings/profile.tsx
@@ -87,7 +87,7 @@ export default function ProfilePage() {
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       exif: false,
