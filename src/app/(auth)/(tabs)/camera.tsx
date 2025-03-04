7140abd6410fa6f162e1f37ca299f1907e92132f
Daniel Supernault <danielsupernault@gmail.com>
2025-03-03 19:53:59 -0700

    Update Camera, fix mediaTypes

--- src/app/(auth)/(tabs)/camera.tsx
+++ src/app/(auth)/(tabs)/camera.tsx
@@ -145,7 +145,7 @@ export default function Camera() {
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsMultipleSelection: true,
       selectionLimit: maxMediaLimit,
       quality: 1,
