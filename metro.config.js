7f6108cccd25c64d2707adee457f05ae1351391e
Daniel Supernault <danielsupernault@gmail.com>
2025-03-13 23:49:49 -0600

    Update metro bundler config

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,13 @@
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const { getDefaultConfig } = require('expo/metro-config')
+const { getDefaultConfig } = require('@expo/metro-config')
+
+const os = require('os');
+if (!os.availableParallelism) {
+  os.availableParallelism = () => Math.max(os.cpus().length - 1, 1);
+}
+
 
 /** @type {import('expo/metro-config').MetroConfig} */
 let config = getDefaultConfig(__dirname, {
