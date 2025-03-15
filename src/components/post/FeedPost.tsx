1c3769209e82492dc6391aa8aa836bf9ce1b0776
Akshay <akshaykale17799@gmail.com>
2025-03-15 14:19:32 +0530

    Biome Fix

--- src/components/post/FeedPost.tsx
+++ src/components/post/FeedPost.tsx
@@ -353,7 +353,7 @@ const PostActions = React.memo(
       Alert.alert(
         'Alt Text',
         post?.media_attachments[idx].description ??
-        'Media was not tagged with any alt text.'
+          'Media was not tagged with any alt text.'
       )
     }
 
@@ -788,7 +788,7 @@ const FeedPost = React.memo(
     const handlePresentModalPress = useCallback(() => {
       bottomSheetModalRef.current?.present()
     }, [])
-    const handleSheetChanges = useCallback((_: number) => { }, [])
+    const handleSheetChanges = useCallback((_: number) => {}, [])
     const renderBackdrop = useCallback(
       (props: BottomSheetBackdropProps) => (
         <BottomSheetBackdrop {...props} disappearsOnIndex={-1} appearsOnIndex={1} />
@@ -881,7 +881,7 @@ const FeedPost = React.memo(
         await Share.share({
           message: post.url || post.uri,
         })
-      } catch (error) { }
+      } catch (error) {}
     }
     return (
       <View flex={1} style={{ width }}>
