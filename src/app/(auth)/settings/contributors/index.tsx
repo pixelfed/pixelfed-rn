49a681b244dd1554bd375ef553c9ec715cdd0956
Akshay <akshaykale17799@gmail.com>
2025-04-05 11:50:52 +0530

    Added search bar

--- src/app/(auth)/settings/contributors/index.tsx
+++ src/app/(auth)/settings/contributors/index.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import {
     H5,
     H4,
+    Input,
     ListItem,
     ScrollView,
     Separator,
@@ -12,9 +13,11 @@ import {
     Text,
     YGroup,
 } from 'tamagui'
+import { useState, useEffect } from 'react'
 
 export default function Screen() {
     const theme = useTheme()
+    const [query, setQuery] = useState('')
 
     const contributors = {
         "Double Platinum Sponsor": ["Ben", "Perkins", "@onequest"],
@@ -30,6 +33,34 @@ export default function Screen() {
         "Sponsors": ["dummy", "dummy", "dummy", "dummy", "dummy", "dummy"],
     }
 
+    const [searchedContributors, setSearchedContributors] = useState(contributors);
+
+    useEffect(() => {
+        if (query === '') {
+            setSearchedContributors(contributors);
+        } else {
+            const newSearchedContributors = {};
+
+            const lowerCasequery = query.toLowerCase();
+
+            for (const key in contributors) {
+                if (Object.hasOwnProperty.call(contributors, key)) {
+                    const originalNamesArray = contributors[key];
+                    const matchingNames = [];
+                    if (Array.isArray(originalNamesArray)) {
+                        for (const name of originalNamesArray) {
+                            if (typeof name === 'string' && name.toLowerCase().includes(lowerCasequery)) {
+                                matchingNames.push(name);
+                            }
+                        }
+                    }
+                    setSearchedContributors(newSearchedContributors);
+                    newSearchedContributors[key] = matchingNames;
+                }
+            }
+        }
+    }, [query]);
+
     return (
         <SafeAreaView
             style={{ flex: 1, backgroundColor: theme.background?.val.default.val }}
@@ -41,30 +72,41 @@ export default function Screen() {
                     headerBackTitle: 'Back',
                 }}
             />
+            <H4 textAlign="center" color={theme.color?.val.default.val} mt="$4" mb="$4">
+                A Heartfelt Thank You to Our Amazing Contributors!
+            </H4>
+            <Input
+                placeholder="Search contributors"
+                m="$3"
+                onChangeText={(text) => setQuery(text)}
+                value={query}
+                bg={theme.background?.val.tertiary.val}
+                color={theme.color?.val.default.val}
+                placeholderTextColor={theme.color?.val.tertiary.val}
+                size="$5"
+            />
             <ScrollView flexShrink={1} showsVerticalScrollIndicator={false}>
                 <YStack p="$5" gap="$4">
-                    <H4 textAlign="center" color={theme.color?.val.default.val} mb="$4">
-                        A Heartfelt Thank You to Our Amazing Contributors!
-                    </H4>
-                    {Object.entries(contributors).map(([sponsorType, names]) => (
-                        <YGroup borderWidth={1} borderColor={theme.borderColor?.val.default.val} key={sponsorType} borderRadius="$8"
-                            separator={<Separator borderColor={theme.borderColor?.val.default.val} />}>
-                            <YGroup.Item>
-                                <ListItem jc="center">
-                                    <H5 color={theme.color?.val.default.val}>{sponsorType}</H5>
-                                </ListItem>
-                            </YGroup.Item>
-                            {names.map((name, index) => (
-                                <YGroup.Item key={index}>
-                                    <ListItem>
-                                        <Text color={theme.color?.val.default.val} textAlign="center">
-                                            {name}
-                                        </Text>
+                    {Object.entries(searchedContributors).filter(([sponsorType, names]) => names.length > 0)
+                        .map(([sponsorType, names]) => (
+                            <YGroup borderWidth={1} borderColor={theme.borderColor?.val.default.val} key={sponsorType} borderRadius="$8"
+                                separator={<Separator borderColor={theme.borderColor?.val.default.val} />}>
+                                <YGroup.Item>
+                                    <ListItem jc="center">
+                                        <H5 color={theme.color?.val.default.val}>{sponsorType}</H5>
                                     </ListItem>
                                 </YGroup.Item>
-                            ))}
-                        </YGroup>
-                    ))}
+                                {names.map((name, index) => (
+                                    <YGroup.Item key={index}>
+                                        <ListItem>
+                                            <Text color={theme.color?.val.default.val} textAlign="center">
+                                                {name}
+                                            </Text>
+                                        </ListItem>
+                                    </YGroup.Item>
+                                ))}
+                            </YGroup>
+                        ))}
                 </YStack>
             </ScrollView>
         </SafeAreaView>
