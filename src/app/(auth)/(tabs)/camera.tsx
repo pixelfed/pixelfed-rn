e9b7c08ea2ccabebb3bef0540c4040df6674c091
Skivling <skiv.impromptu166@passinbox.com>
2025-09-28 09:03:53 +1300

    add short notice

--- src/app/(auth)/(tabs)/camera.tsx
+++ src/app/(auth)/(tabs)/camera.tsx
@@ -289,6 +289,7 @@ export default function Camera() {
   }
 
   const warningMessage = () => {
+    const isPrivateAccount = userSelf && userSelf?.locked
     if (!isSensitive && scope == 'public') {
       return
     }
@@ -299,14 +300,18 @@ export default function Camera() {
       if (scope == 'unlisted') {
         return 'You marked this post as sensitive and unlisted, it will be hidden from public timelines and behind a warning before media is displayed.'
       }
-      if (scope == 'private') {
+      if (isPrivateAccount && scope == 'private') {
+        return 'You marked this post as sensitive and private, it will only be shared to your followers and be hidden behind a warning before media is displayed. Creating public posts is disabled for private accounts.'
+      } else if (scope == 'private') {
         return 'You marked this post as sensitive and private, it will only be shared to your followers and be hidden behind a warning before media is displayed.'
       }
     } else {
       if (scope == 'unlisted') {
         return 'You marked this post as unlisted, it will be hidden from public timelines.'
       }
-      if (scope == 'private') {
+      if (isPrivateAccount && scope == 'private') {
+        return 'This post is marked as private, it will only be shared to your followers. Creating public posts is disabled for private accounts.'
+      } else if (scope == 'private') {
         return 'You marked this post as private, it will only be shared to your followers.'
       }
     }
